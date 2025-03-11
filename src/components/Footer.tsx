ed4c4fb52c3b3d0536e98a4d168ac9a2b9eb5873
leonardoDluz <thestuff.luz@gmail.com>
2025-03-11 18:08:15 -0300

    footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,22 @@
 export function Footer() {
   return (
-    <footer className="bg-black h-40">
-
+    <footer className="bg-black text-white px-2.5 py-3.5" >
+      <div className="pb-2.5">
+        <h5 className="font-bold">Autores:</h5>
+        <ul className="pl-2">
+          <li><a href="https://github.com/leonardoDluz/">Leonardo Luz</a></li>
+          <li><a href="https://github.com/Leo2828">Leonardo Cesar</a></li>
+          <li><a href="https://github.com/Vitor-G-Cavalheiro">Vitor Cavalheiro</a></li>
+        </ul>
+      </div>
+      <div>
+        <h5 className="font-bold">Referências:</h5>
+        <ul className="pl-2">
+          <li><a href="#">Lorem ipsum</a></li>
+          <li><a href="#">Lorem ipsum</a></li>
+          <li><a href="#">Lorem ipsum</a></li>
+        </ul>
+      </div>
     </footer>
   )
 }
